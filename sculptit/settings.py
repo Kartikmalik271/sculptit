5e38f8157dd3730eb379d1d6cef8a7790a1a8738
Kartik Malik <kartikmalik271@gmail.com>
2021-04-20 17:48:13 +0530

    try

--- sculptit/settings.py
+++ sculptit/settings.py
@@ -154,7 +154,6 @@ REST_FRAMEWORK = {
 CORS_ORIGIN_ALLOW_ALL = True
 
 django_heroku.settings(locals())
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
